925678cf45a91b2fd348c0cf24dab5e7c0262492
NTaylor23 <nicholaswtaylor@proton.me>
2025-12-03 22:40:08 -0800

    day04

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 Note: Puzzle inputs are not committed to this repository. No LLM tools are being used.
 
-| Day | Name            | File                  |
-| --- | --------------- | --------------------- |
-| 01  | Secret Entrance | [day01.py](/day01.py) |
-| 02  | Gift Shop       | [day02.py](/day02.py) |
-| 03  | Lobby           | [day03.py](/day03.py) |
+| Day | Name                | File                  |
+| --- | ------------------- | --------------------- |
+| 01  | Secret Entrance     | [day01.py](/day01.py) |
+| 02  | Gift Shop           | [day02.py](/day02.py) |
+| 03  | Lobby               | [day03.py](/day03.py) |
+| 04  | Printing Department | [day04.py](/day04.py) |
---
+++ day04.py
@@ -0,0 +1,30 @@
+from session import AdventOfCodeSession
+
+session = AdventOfCodeSession(day=4)
+adj = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, -1), (1, 1), (1, -1), (-1, 1)]
+data = session.puzzle_input.splitlines()
+
+p1 = p2 = 0
+initial_rolls = set()
+for y, row in enumerate(data):
+    for x, col in enumerate(row):
+        if col == "@":
+            initial_rolls.add((y, x))
+
+removed_rolls = set()
+while True:
+    flag = False
+    current_rolls = initial_rolls - removed_rolls
+
+    for y, x in current_rolls:
+        neighbors = map(lambda d: (y + d[0], x + d[1]) in current_rolls, adj)
+        if sum(neighbors) < 4:
+            removed_rolls.add((y, x))
+            flag = True
+
+    p1 = p1 or len(removed_rolls)
+    if not flag:
+        break
+
+p2 = len(removed_rolls)
+print(p1, p2)
